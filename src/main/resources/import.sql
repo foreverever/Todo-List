d00be75c6d524c883efed8e373bc7debea1a773f
TaeSu <lollolzkk@gmail.com>
2019-05-20 05:11:24 +0900

    modify import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO TODO (id, created_date, modified_date, title, contents, completed, priority, expired) VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '빨래', '운동복 빨기', false, '중간', false);
 INSERT INTO TODO (id, created_date, modified_date, title, contents, completed, priority, expired) VALUES (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '운동', '상체 운동', false, '중간', false);
-INSERT INTO TODO (id, created_date, modified_date, title, contents, completed, priority, expired) VALUES (3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '과제', 'TodoList 만들기', false, '높음', false);
+INSERT INTO TODO (id, created_date, modified_date, title, contents, completed, priority, expired, deadline) VALUES (3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '과제', 'TodoList 만들기', false, '높음', false, '2019-05-20 23:59:00');
 INSERT INTO TODO (id, created_date, modified_date, title, contents, completed, priority, expired) VALUES (4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '방청소', '책상 정리', false, '낮음', false);
